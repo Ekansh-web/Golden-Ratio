3c03c0e5f586352ba631c5cbbbcefa9dbba340cd
Ekansh Pandey <65842151+Ekansh-web@users.noreply.github.com>
2020-08-28 21:47:06 +0530

    Add files via upload

---
+++ fibgame.php
@@ -0,0 +1,116 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+         
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <title>Golden Ratio</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <?php if(isset($_POST['numo'])||isset($_POST['test'])) { ?>
+       <script>
+	$(document).ready(function(){
+		$("#loginModal").modal('show');
+	});
+    </script> <?php } ?>
+    
+    </head>
+    <body>
+       
+       <?php include 'head.php'; 
+       if(isset($_POST['numo'])){
+              $a = $_POST['numo'];
+              $b = $_POST['numt'];
+       }
+       ?>
+        
+        <div style="margin-top: 58px;">
+            <img src="img/Ram.jpg_large" width="65%"/><img src="img/gratiog.gif" width="35%"/>
+        </div>
+        <br>
+        <br>
+        <div class="content"></div>
+        <br>
+        
+        <div style="background-color: #17202A" class="container">
+            <center>
+                <h2 style="font-family: cursive; color: #D4AC0D"><b>HERE IS A SIMPLE MAGICAL ACTIVITY</b></h2>
+            </center>
+            <br>
+            <center>
+            <p style="font-family: 'Lobster', cursive; font-size: 23px; color: #ffffff">Everything in this world has a relation with the maths. We can take the example of Golden Ratio in maths which can be
+                found in many different fields, which you can see in the examples tab.<br><br>
+            Now we will see that how any two number of our choice of the maths can give the Golden Ratio. It seems magical, yes it is.
+            Lets start this game of Golden Ratio and take any two random numbers...</p>
+            </center>
+            <center style="margin-top: 50px; margin-bottom: 30px;"><a href="#"data-toggle="modal" data-target="#loginModal" role="button" class="btn btn-primary">START</a></center>
+        </div>
+        <br>
+        <br>
+        <div class="content"></div><br>
+        <div class="content"></div>
+        <br>
+        <div class="container"><center><a href="index.php" style="float: left; font-size: 16px;">Home</a><a href="examples.php" style="float: right; font-size: 16px">Application Golden Ratio</a></center></div>
+        <br>
+        
+         <div class="modal fade" id="loginModal" data-keyboard="false" data-backdrop="static">
+             
+              <div class="modal-header other">
+                  <center><h2 class="modal-title texter"><b>Golden Ratio</b></h2></center>
+              </div>
+             
+             <div class="modal-body">
+                     
+                 <div style="color: #EFF5F5; background-color:rgba(0, 0, 0, 0.7);" class="col-sm-6 col-sm-offset-3 col-xs-12">   
+                     
+<!--if-->       <?php if(!isset($_POST['numo'])) { ?>
+                    <br><h4 style="font-family: cursive"><b style="color:#B03A2E;">NOTE : </b>You can take the same numbers as well as you can also take the zero in one of the section below but you can not take zero in both the sections below (0/0 not exists)</h4>
+                     <br><br>
+                     <form method="POST" action="">
+                         <label for="num">Enter First Number :</label>
+                         <input type="number" class="form-control" name="numo" id="num" placeholder="enter any random first number" required="true"><br>
+                         
+                         <label for="snum">Enter Second Number :</label>
+                         <input type="number" class="form-control" name="numt" id="snum" placeholder="enter any random second number" required="true"><br>
+                     <button type="submit" class="btn btn-primary" value="submit">Go</button> <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                     </form>
+<!--else-->         <?php } else {
+                         if(($a==0)&&($b==0)) { ?>
+                            <br><h4><b>Something that doesn't exist will never provide the Golden ratio. You can not take the zero in both the fields</b></h4><br><br>
+                            <form method="POST" action="">
+                               <button type="submit" class="btn btn-success" name="test" value="1"><span class="glyphicon glyphicon-arrow-left"></span> Go back</button>
+                            </form>
+                   <?php } else { ?>
+                            <h3 style="font-family: cursive; color: #B03A2E"><center>This is up to 20 and the ratio of last two numbers will be GOLDEN RATIO</center></h3>
+                            <?php for($i=0;$i<20;$i++) {
+                                   $z = $a;
+                                   $c = $b;
+                                   $b = $a + $b;
+                                   $a = $c;?>
+                            <h4><b style="color: #B03A2E"><?php echo $i+1;?> - </b><center><?php echo $z;?><b style="color: #B03A2E"> + </b><?php echo $c; ?><b style="color: #B03A2E"> = </b><?php echo $b; ?>
+                                </center></h4>
+                            <div class="cont"></div>
+                               <?php } $ratio = $b/$a;?>
+                            <br><center><p style="font-size: 16px;">RATIO <span class="glyphicon glyphicon-arrow-right"></span> <?php echo $b;?><b style="color: #B03A2E"> / </b><?php echo $a;?>
+                                    <b style="color: #B03A2E"> = </b><b style="color: #D4AC0D"><?php echo $ratio;?></b></p></center>
+                            <br>
+                            <center>
+                                <h4>We are simply taking <b style="color: #B03A2E">number 1</b> and <b style="color: #B03A2E">number 2</b> and on add we got <b style="color: #B03A2E">number 3</b> then we add <b style="color: #B03A2E">number 2</b> and <b style="color: #B03A2E">number 3</b> and we got <b style="color: #B03A2E">number 4</b> and so on.
+                                    After doing this <b style="color: #B03A2E">20 times</b> the ratio of the last two numbers will be the <b style="color:#D4AC0D">GOLDEN RATIO</b></h4>
+                                <br>
+                           <form method="POST" action="">
+                               <button type="submit" class="btn btn-primary" name="test" value="1"><span class="glyphicon glyphicon-arrow-left"></span>Again Try</button> <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </form>
+                            </center><br><br>
+                        <?php }} ?>
+                 </div>
+             </div>
+         </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,120 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+         
+         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <title>Golden Ratio</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    
+    
+    <body>
+       
+       <?php include 'head.php'; ?>
+        
+        <div style="margin-top: 58px;">
+        <img src="img/spi.jpg" width="56%"/><img src="img/golden-ratio-in-nature.jpeg" width="44%"/>
+        </div>
+        
+        <!-- What is Golden-->
+        <br>
+        <br>
+        <div class="content"></div>
+        <br>
+        
+        <div class="container" style=" border-top-style: solid; border-top-width: 2px; border-top-color: #DAD8D8" id="bottomline">
+            <h2 style="font-family: cursive; color: #D4AC0D">What is Golden Ratio ?</h2><br>
+            <p style="font-size: 16px;">Had you ever thought that why <b>1.61803398...</b> can be so important to us? What makes this single number so valuable that we call it the most important number of
+                this universe?<br>"<u>The Golden Ratio Has Inspired Thinkers Of All Disciplines Like No Other Number In The History Of Mathematics</u>." One Of The Greatest
+                Astrophysicist Of All Times, <b>Mario Livio</b> Once Quote These Lines About The Golden Ratio.
+                <b>Mario Livio</b> tells the tale of a number at the heart of that mystery: <b>phi, or 1.6180339887...</b>This curious mathematical relationship, widely known as "<b><i>The Golden
+                Ratio</i></b>" was discovered by <b>Euclid</b> more than two thousand years ago because of its crucial role in the construction of the pentagram, to which <b>magical
+                properties</b> had been attributed.
+            </p>
+        
+    <center class="col-xs-12">
+        <br>
+        <p style="color: #D4AC0D"><span class="glyphicon glyphicon-arrow-down"></span> This picture shown is a summary of Golden Ratio <span class="glyphicon glyphicon-arrow-down"></span></p>
+        <img src="img/The-Golden-Ratio-Graphic-01.webp" width="75%"/>
+    </center>
+        </div>
+        
+        <!-- How this exp -->
+        <br>
+        <div class="content"></div>
+        <br>
+        <div class="container" id="bottomline">
+            <h2 style="font-family: cursive; color: #D4AC0D">How this expression comes ?</h2><br>
+    <center>
+        <p style="font-size: 16px;">Here is a diagram of rectangle containing sides phi and 1<br>When we divide this rectangle into a square then automatically another rectangle created
+        that is of side (phi-1) and 1.</p>
+        <img src="img/1G.gif" /><br><br>
+        <p style="font-size: 16px;">Expression formed for golden ratio :</p>
+        <img src="img/2G.gif" /><br>
+        <img src="img/3G.gif" /><br><br>
+        <p style="font-size: 16px;"><b>To understand the above example. Let's understand this simple one</b><br>Here AB is a line of length phi+1 where C is point on line<br>so, AC = phi and CB=1</p>
+        <img src="img/4G.gif" /><br><br>
+        <p style="font-size: 14px;">Expression formed :</p>
+        <img src="img/5G.gif" /><br><br>
+        <p style="font-size: 14px;">where, AB = phi+1, AC = phi and CB=1</p>
+        <img src="img/6G.gif" /><br>
+        <img src="img/7G.gif" /><br><br>
+        <p style="font-size: 14px;">on applying Sridharacharya formula :</p>
+        <img src="img/8G.gif" /><br><br>
+        <p style="font-size: 14px;"><b>(GOLDEN RATIO)</b> Value comes:</p>
+        <img src="img/9G.gif" />
+    </center>
+    </div>
+        
+        <!--How one can-->
+       <br>
+        <div class="content"></div>
+        <br> 
+        
+        <div class="container" id="bottomline">
+            <h2 style="font-family: cursive; color: #D4AC0D">How one can see GOLDEN RATIO in nature ?</h2><br>
+            <p style="font-size: 16px;">Now taking the <b>first example of rectangle</b> where we got the square and a rectangle after dividing, there also we have got the same expression of Golden Ratio<br>
+            Now if we make further squares in rectangles which are coming on dividing then they also satisfy the expression of Golden Ratio...</p>
+            <center><img src="img/image3.jpg" width="50%" /></center><br>
+            <center><img src="img/image3-con.jpg" width="50%"/></center>
+            <h4 style=" font-family: cursive">GOLDEN RATIO in real world :</h4>
+            <img src="img/Galaxy-Spiral-1024x852.jpg" width="46%"/> <img src="img/image2.webp" width="52%"/><br><br>
+        </div>
+        <br>
+        <div class="content"></div>
+        <br> 
+        
+        <!-- Examples -->
+        <div id="bottomline">
+            <center><h4 style="font-family: cursive; color: #D4AC0D"><span class="glyphicon glyphicon-arrow-down"></span> More Examples <span class="glyphicon glyphicon-arrow-down"></span></h4></center>
+    <img src="img/giphy (1).gif" width="50%"/><img src="img/face.webp" width="50%"/>
+    <br><br>
+    <img src="img/im1.jpg" width="13%"/><img src="img/im2.jpg" width="13%"/><img src="img/im3.jpg" width="13%"/><img src="img/immiddle.jpg" width="22%"/><img src="img/im4.jpg" width="13%"/><img src="img/im5.jpg" width="13%"/><img src="img/im6.jpg" width="13%"/>
+        </div>
+        
+        <!--youtube-->
+        <br>
+        <br>
+        <div class="content"></div>
+        <br>
+        <div id="bottomline" class="container">
+            <h5 style="font-family: cursive">You can also take the help of this video : </h5>
+    <center>
+        <iframe width="50%" height="350px" src="https://www.youtube.com/embed/CSoHCHQ3zJw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </center>
+        </div>
+        <br>
+        <div class="container"><center><a href="examples.php" style="float: left; font-size: 16px;">Application Golden Ratio</a><a href="fibgame.php" style="float: right; font-size: 16px">Magic of Golden ratio</a></center></div>
+        <br>
+        <div class="content"></div>
+    <br>
+    </body>
+</html>
+
+
