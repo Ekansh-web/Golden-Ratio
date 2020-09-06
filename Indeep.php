eaf2c68f4c1d975e191a2d76997b56c47641ee16
Ekansh Pandey <65842151+Ekansh-web@users.noreply.github.com>
2020-09-06 19:33:13 +0530

    Add files via upload

---
+++ Indeep.php
@@ -0,0 +1,97 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+         
+         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <title>Golden Ratio | In depth</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" type="image/icon-type" href="img/icon.svg">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    
+    
+    <body>
+       
+       <?php include 'head.php'; ?>
+        
+        <div style="margin-top: 58px;">
+            <img src="img/In deep/blackhole.jpg" width="50%"/><img src="img/In deep/tornado.gif" width="50%"/>
+        </div>
+         <br>
+        <br>
+        <div class="content"></div>
+        
+       
+        <div class="container">
+            <h3 style="font-family: cursive; color: #D4AC0D"><u>Spiral formation in Atomic Model (Rutherford Limitation)</u></h3><br>
+            <center><img src="img/In deep/rutherf.jpg" width="35%"/></center>
+            <p style="font-size: 16px;">Continuous radiations of energy causes loss of energy which would slow down the speed of the
+                electron and eventually the electron would fall into the nucleus atom would collapse. But such collapse
+                does not occur.<br>Formation of spiral can be seen in many areas of science and many scientist come up with the spirals</p>
+            <center><img src="img/In deep/elec_nucleus_em.png" width="80%"/></center>
+        </div>
+        <br>
+        <div class="content"></div>
+        <br>
+        <div class="container">
+            <h3 style="font-family: cursive; color: #D4AC0D"><u>In Human Body</u></h3><br>
+            <p style="font-size: 16px;">Spiral can be seen in different areas of body whether in the making of fist or in the growth of hair..<br>You can also read this research paper of University of Dublin on <a href="https://www.yumpu.com/en/document/read/26149498/application-of-the-golden-ratio-to-3d-facial-models" target="blank">Application of the Golden Ratio to 3D Facial Models</a></p>
+            <center><img src="img/In deep/hair.jpg" width="30%"/><img src="img/In deep/hand_spiral.jpg" width="30%"/><img src="img/In deep/eye.webp" width="30%"/></center><br>
+            <br>
+            <div class="content"></div>
+            <br>
+            <center><p style="font-size: 16px;">While in researches, Golden Ratio was also found in <b>Cochlea</b> ear bone</p></center>
+            <img src="img/In deep/cochlea_position.png" width="50%"/><img src="img/In deep/cochlea_Structure.png" width="50%"/>
+            <br>
+                <div class="content"></div>
+            <br>
+            <center><p style="font-size: 16px;">When it comes to the formation of <b>structure of DNA</b> then there also the GOLDEN RATIO can be easily seen</p></center>
+            <center><img src="img/In deep/dna_exp.jpg" width="25%"/> <img src="img/In deep/dna.jpg" width="30%"/></center>
+        </div>
+        <br>
+        <div class="content"></div>
+        <br>
+        
+        <div class="container">
+            <h3 style="font-family: cursive; color: #D4AC0D"><u>In Hypnotism</u></h3><br>
+            <p style="font-size: 16px;">In different areas of fields, Golden Ratio has shown different effects and now it is clear that the Golden Ratio
+                is such thing that can take the equation to infinity i.e something that relates to infinite.</p>
+                <center><img src="img/In deep/hyp0.jpg" width="41%"/><img src="img/In deep/hypno1.jpg" width="59%"/></center><br>
+                <p style="font-size: 16px;">In Hypnotism -: You can see the below example of spiral <b>by coming nearer and going away continuously from screen..</b></p>
+                <center><tt>Don't see it for a long duration</tt></center>
+                <center><img src="img/In deep/hypno.jpg" width="50%"/></center>
+        </div>
+        <br>
+        <div class="content"></div>
+        <br>
+        
+         <div class="container">
+             <h3 style="font-family: cursive; color: #D4AC0D"><u>Relation Fibonacci-Golden Ratio</u></h3>
+             <p style="font-size: 16px;">Fibonacci Numbers are the numbers in a sequence to infinite. There is a=0 and b=1 then a+b There is a=0 and b=1 then a+b i.e 1 will be the next one and now a=1 and b=1 then a+b=2 will be the next one and so on....</p>
+             <center><img src="img/In deep/numfib.jpg" width="50%"/></center><br>
+             <p style="font-size: 16px;">The ratio of adjacent Fibonacci numbers will be Golden Ratio. Greater the Fibonacci Number more accurate will be the ratio</p>
+             <center><img src="img/In deep/numfib1.png" width="50%"/></center><br>
+         </div>
+        <br>
+        <div class="content"></div>
+        <br>
+        
+        <div class="container">
+            <h3 style="font-family: cursive; color: #D4AC0D"><u>We can find Golden Ratio in many areas from Black hole to Periodic table</u></h3><br>
+         </div>
+        <img src="img/In deep/periodicTable.gif" width="33%"/><img src="img/In deep/milkyway.png" width="34%"/><img src="img/In deep/cyc_spiral.jpg" width="33%"/>
+        <br>
+        <br>
+        <div class="content"></div>
+        <br>
+        <div class="content"></div>
+        <br>
+        <div class="container"><center><a href="index.php" style="float: left; font-size: 16px;">Home</a><a href="examples.php" style="float: right; font-size: 16px">Application Golden Ratio</a></center></div>
+        <br>
+    </body>
+</html>
---
+++ head.php
@@ -0,0 +1,20 @@
+ <nav style="background-color: #17202A; margin: 1px; padding: 2px;" class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    <a style="font-family: cursive; color: #D4AC0D;" href="index.php" class="navbar-brand"><b>GOLDEN RATIO</b></a>
+            </div>
+                 <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                    <li><a href="examples.php"><span class="glyphicon glyphicon-picture"></span> Examples</a></li>
+                    <li><a href="Indeep.php"><span class="glyphicon glyphicon-pencil"></span> In Depth</a></li>
+                    <li><a href="fibgame.php"><span class="glyphicon glyphicon-tower"></span> Magic of Golden Ratio</a></li>
+                </ul>
+              </div>
+            </div>
+        </nav>
